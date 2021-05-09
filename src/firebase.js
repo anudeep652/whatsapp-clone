0616d683df9037fdc335a4b6cf783f0146c4c2e9
anudeep652 <76202842+anudeep652@users.noreply.github.com>
2021-05-09 21:09:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCK4FwsvRIjVbcydtvlFFDYMezPosEEa2g",
-  authDomain: "whatspp-clone-ac798.firebaseapp.com",
-  projectId: "whatspp-clone-ac798",
-  storageBucket: "whatspp-clone-ac798.appspot.com",
-  messagingSenderId: "279493179606",
-  appId: "1:279493179606:web:15d85249a83f2bbcd8b177",
-  measurementId: "G-B554RW7YQH"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
